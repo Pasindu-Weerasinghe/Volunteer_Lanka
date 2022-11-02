69435167296cfa67e7b465cf47550a435210116c
n-imanya <nethimanya2000@gmail.com>
2022-11-02 17:38:03 +0530

    Login

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php include 'Components/Navbar/navbar.php' ?>
+<?php include 'Navbar/navbar.php' ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Volunteer Lanka</title>
+</head>
+<body>
+<?php include'cards/cards.php'?>
+<?php include'cards/cards.php'?>
+<?php include'cards/cards.php'?>
+</body>
+</html>

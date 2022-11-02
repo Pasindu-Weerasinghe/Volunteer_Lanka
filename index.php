b91fdecf3052a21d88de5d5d5ec5c94a325bc368
Binula Rasanjith <binula28@gmail.com>
2022-11-02 20:45:57 +0530

    changing files

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<?php include 'Navbar/navbar.php' ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Searchbar.css">
-    <title>Document</title>
+    <title>Volunteer Lanka</title>
 </head>
 <body>
-    <h1 class="my">Pasindu</h1>
+<?php include'cards/cards.php'?>
+<?php include'cards/cards.php'?>
+<?php include'cards/cards.php'?>
 </body>
 </html>

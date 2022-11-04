862ce09ecc01489fc3a6b94f15a7d2f2710ed358
Binula Rasanjith <binula28@gmail.com>
2022-11-04 11:59:46 +0530

    minor changes

--- login.php
+++ login.php
@@ -2,28 +2,31 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="Login/login.css">
     <title>Login</title>
-    <body>
+
+<body>
     <form action="login.php" method="post">
-    <div class="container">
-        <h1>Login</h1>
-        <label for="uname"><b>Username</b></label>
-        <input type="text" name="uname" required>
+        <div class="container">
+            <h1>Login</h1>
+            <label for="uname"><b>Username</b></label>
+            <input type="text" name="uname" required>
 
-        <label for="psw"><b>Password</b></label>
-        <input type="password" name="psw" required>
-        <span class="psw">Forgot <a href="#">password?</a></span>
+            <label for="psw"><b>Password</b></label>
+            <input type="password" name="psw" required>
+            <span class="psw">Forgot <a href="#">password?</a></span>
 
-        <button type="submit">Login</button>
-    </div><br/>
+            <button type="submit">Login</button>
+        </div><br />
 
-    <span class="signup">Don't have an account? <a href="#">Sign up</a></span>
+        <span class="signup">Don't have an account? <a href="#">Sign up</a></span>
     </form>
-    </body>
+</body>
 </head>
+
 </html>

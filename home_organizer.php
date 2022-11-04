b6681644ef22b0e25289b7536bea9a4fc1b87578
Binula Rasanjith <binula28@gmail.com>
2022-11-04 18:33:42 +0530

    organizer home page added

---
+++ home_organizer.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cards/cards.css">
+    <title>Home</title>
+</head>
+
+<body>
+    <?php include 'Navbar/navbar.php' ?>
+
+    <br />
+    <h2>Upcoming Projects</h2><br /><br />
+    <section class="container">
+        <div class="card">
+            <div class="card-image card1">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="upcoming_volunteer.php">View</a>
+        </div>
+        <div class="card">
+            <div class="card-image card2">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="completed_volunteer.php">View</a>
+        </div>
+        <div class="card">
+            <div class="card-image card3">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="">View</a>
+        </div>
+    </section><br />
+    <hr><br />
+    <h2>Completed Projects</h2><br /><br />
+    <section class="container">
+        <div class="card">
+            <div class="card-image card1">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="">View</a>
+        </div>
+        <div class="card">
+            <div class="card-image card2">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="">View</a>
+        </div>
+        <div class="card">
+            <div class="card-image card3">
+            </div>
+            <h2>Project Name</h2>
+            <a class="btn" href="">View</a>
+        </div>
+    </section>
+
+</body>
+
+</html>

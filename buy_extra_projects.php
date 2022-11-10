f1bdbb98172664344792eb7a173d3af850e30330
Binula Rasanjith <binula28@gmail.com>
2022-11-10 16:20:37 +0530

    buy extra projects (may need changes)

---
+++ buy_extra_projects.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/view.css">
+    <title>Buy Extra Project</title>
+</head>
+
+<body>
+    <h2>Payments for Organizing Extra Projects</h2><br />
+    <div class="container">
+        <p>You have exceeded your limit per month to organize projects. To organize any extra projects you have to pay LKR 80.00 per project.</p>
+        <p>Buying 5 projects will facilitate you to cancel one project and postpone one project.</p>
+        <table>
+            <tr>
+                <td>Number of projects</td>
+                <td><input type="number" name="v-name"></td>
+            </tr>
+            <tr>
+                <td>Amount</td>
+                <td><?php echo 345; ?></td>
+            </tr>
+        </table>
+    </div>
+
+    <button class="btn1" href="">Back</button>
+    <button class="btn2" href="">Pay</button>
+
+</body>
+
+</html>

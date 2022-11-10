8960db925c82cabdc583b64b092c73b4a9734326
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2022-11-11 00:05:16 +0530

    changes

--- view_sponsor_notices.php
+++ view_sponsor_notices.php
@@ -8,49 +8,78 @@
 </head>
 
 <body>
-    <br><h2>Sponsor Notices</h2><br>
-    <div class="slider">
-        <span id="slide-1"></span>
-        <span id="slide-2"></span>
-        <span id="slide-3"></span>
-        <div class="image-container">
-            <img src="cards/img1.jpg" class="slide" width="500" height="300" />
-            <img src="cards/img2.jpg" class="slide" width="500" height="300" />
-            <img src="cards/img3.jpg" class="slide" width="500" height="300" />
+    <br>
+    <h2>Sponsor Notices</h2><br>
+    <div>
+        <div class="slider">
+            <span id="slide-1"></span>
+            <span id="slide-2"></span>
+            <span id="slide-3"></span>
+            <div class="image-container">
+                <img src="cards/img1.jpg" class="slide" width="500" height="300" />
+                <img src="cards/img2.jpg" class="slide" width="500" height="300" />
+                <img src="cards/img3.jpg" class="slide" width="500" height="300" />
+            </div>
+            <div class="buttons">
+                <a href="#slide-1"></a>
+                <a href="#slide-2"></a>
+                <a href="#slide-3"></a>
+            </div>
         </div>
-        <div class="buttons">
-            <a href="#slide-1"></a>
-            <a href="#slide-2"></a>
-            <a href="#slide-3"></a>
-        </div>
-    </div>
 
-    <table class="table1">
+
+        <table class="table1">
             <tr>
                 <td>Date</td>
                 <td>: 11/10/2022</td>
-
-            </tr><br>
+            </tr>
             <tr>
                 <td>Time</td>
                 <td>: 10.00 AM</td>
-
             </tr>
             <tr>
                 <td>Venue</td>
                 <td>: Galle</td>
-
+            </tr>
+            <tr>
+                <td>Organizer</td>
+                <td>: Rotaract Club UOM</td>
             </tr>
             <tr>
                 <td>Number of Volunteers</td>
-                <td>: 20</td>
-
+                <td>: 5</td>
             </tr>
             <tr>
                 <td>Description</td>
                 <td>:________________________________________________</td><br>
-
             </tr>
         </table>
+    </div>
+
+    <p>Select sponsor package bellow</p><br><br>
+
+    <div class="silver">
+        <input type="radio" name="individual">
+        <strong>Silver</strong><br>
+        <strong>Price: 2000.00</strong> 
+    </div>
+
+    <div class="silver">
+        <input type="radio" name="individual">
+        <strong>Gold</strong><br>
+        <strong>Price: 1000.00</strong> 
+    </div>
+
+    <div class="silver">
+        <input type="radio" name="individual">
+        <strong>Platinum</strong><br>
+        <strong>Price: 500.00</strong> 
+    </div>
+
+    <div>
+        <br><br>
+        <button class="btn1"><a href="#">Cancel</a></button>
+        <button class="btn2"><a href="#">Confirm</a></button>
+    </div>
 
     </html>

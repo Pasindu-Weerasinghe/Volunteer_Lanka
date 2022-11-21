1f69b0128e926504f34f2ad440dcaf14854c40c0
Binula Rasanjith <binula28@gmail.com>
2022-11-21 21:12:02 +0530

    changes

--- login.php
+++ login.php
@@ -56,7 +56,7 @@ if (isset($_POST['login'])) {
     <form action="login.php" method="post">
         <div class="container">
             <h1>Login</h1>
-            
+
             <?php
             if ($error == 'incorrect') {
                 echo '<p id="error">Incorrect Username/Password</p>';
@@ -64,7 +64,7 @@ if (isset($_POST['login'])) {
             if ($error == 'restricted') {
                 echo '<p id="error">Your account is restricted!</p>';
             }
-            ?>
+        ?>
 
             <label for="uname"><b>Username</b></label>
             <input type="email" name="uname" placeholder="Email" required>

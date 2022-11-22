1ac57e3d0f932b653be41dba06c1f23f6a070c94
Pasindu-Weerasinghe <91451835+Pasindu-Weerasinghe@users.noreply.github.com>
2022-11-22 19:48:04 +0530

    change some css admin_home.php

--- Navbar/navbar.php
+++ Navbar/navbar.php
@@ -14,7 +14,6 @@
         <nav>
             <ul>
                 <li><a href="/home">Home</a></li>
-                <li><a href="/services">Services</a></li>
                 <li><a href="/about">About Us</a></li>
                 <li><a href="/contact">Contact Us</a></li>
                 <li id="login"><a href="login.php">LOGIN</a></li>
@@ -28,7 +27,6 @@
                 <div class="bar3"></div>
                 <ul class="mobile-menu">
                     <li><a href="/home">Home</a></li>
-                    <li><a href="/services">Services</a></li>
                     <li><a href="/about">About Us</a></li>
                     <li><a href="/contact">Contact Us</a></li>
                     <li id="loginmobile"><a href="/login">LOGIN</a></li>
--- admin_home.php
+++ admin_home.php
@@ -1,4 +1,4 @@
-<?php include ('Navbar/navbar.php');?>
+<?php include ('Navbar/navbar.php'); ?>
 <?php include 'conn.php';?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/admin_home.css">
+    <title>Admin Home Page</title>
 </head>
 <body>
     <br><br><br>

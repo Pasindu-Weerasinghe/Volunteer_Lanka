2b074ddf0b2e7e4dcedc184ea626aa05e89553e9
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2022-11-25 11:42:43 +0530

    changes

--- signup_auth.php
+++ signup_auth.php
@@ -1,6 +1,6 @@
 <?php 
 include 'conn.php';
-$error=null;
+/*$error=null;*/
 
 if (isset($_REQUEST["signup"])){
 
@@ -10,36 +10,35 @@ if (isset($_REQUEST["signup"])){
     $name = $_POST['name'];
     $contact = $_POST['contact'];
     $address = $_POST['address'];
+
     $type = $_POST['type'];
     $confirm = $_POST['confirm-psw'];
-    $uid = $row['U_ID'];
+   
 
     if($confirm==$psw)
     {
-        $query1 = "INSERT INTO user (U_ID, Email, Password, Role, Status, Restricted) values ('$uid', '$email', '$psw', '$role','active', '0')";
-    $result1 = mysqli_query($conn, $query1);
+        $query1 = "INSERT INTO user (Email, Password, Role, Status, Restricted) values ('$email', '$psw', '$role','active', '0')";
+        $result1 = mysqli_query($conn, $query1);
 
-    $query2 = "SELECT U_ID FROM user WHERE Email = '$email'";
-    $result2 = mysqli_query($conn, $query2);
-    $row = mysqli_fetch_array($result2);
+        $query2 = "SELECT U_ID FROM user WHERE Email = '$email'";
+        $result2 = mysqli_query($conn, $query2);
+        $row = mysqli_fetch_array($result2);
+        $uid = $row['U_ID'];
     
 
     $query1 = "INSERT INTO sponsor (U_ID, Name, Address, Contact, Type ) values ('$uid','$name','$address', '$contact', '$type')";
     $result1 = mysqli_query($conn, $query1);
     }
+    
     else
     {
         $error="Password is not matched!";
 
     }
 
-    
-
-
 }   
 
 
 session_destroy();
 $conn->close();
-header("Location:login.php")
-?>
+header("Location:login.php");

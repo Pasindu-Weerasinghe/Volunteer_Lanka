35b04c169ac7ef81fef7b0f0c599df4db2256bfe
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2022-11-28 13:07:36 +0530

    changes

--- upload_image.php
+++ upload_image.php
@@ -5,19 +5,20 @@ session_start();
 if (isset($_REQUEST["request"])){
 
     $uid = $_SESSION['uid'];
-    $location = $_POST['location'];
     $description = $_POST['des'];
+    $status = $_POST['status'];
+    $sponsor = $_POST['sponsor'];
  
     $targetDir = "images/";
     $allowTypes = array('jpg','png','jpeg','gif');
 
-    $query = "INSERT INTO pr_ideas (Description, Location, U_ID) VALUES ('$description', '$location', '$uid')";
+    $query = "INSERT INTO advertisment (Description, status, sponsor) VALUES ('$description', '$status', '$sponsor')";
     $result = mysqli_query($conn, $query);
 
-    $query2 = "SELECT PI_ID FROM pr_ideas WHERE U_ID = '$uid' && Location = '$location'";
+    $query2 = "SELECT AD_ID FROM advertisment WHERE U_ID = '$uid'";
     $result2 = mysqli_query($conn, $query2);
     $row = mysqli_fetch_array($result2);
-    $idea = $row['PI_ID'];
+    $idea = $row['AD_ID'];
     print($idea);
 
     if (!empty($_FILES["file"]["name"])) {
@@ -31,7 +32,7 @@ if (isset($_REQUEST["request"])){
             if(in_array($fileType, $allowTypes)){
                 if(move_uploaded_file($_FILES["file"]["tmp_name"][$i], $targetFilePath)){
                     
-                    $query3 = "INSERT into idea_image (PI_ID, Image) VALUES ('".$idea."','".$fileName."')";
+                    $query3 = "INSERT into ad_image (AD_ID, Image) VALUES ('".$idea."','".$fileName."')";
                     $result3 = mysqli_query($conn, $query3);
                     if($result3){
                         $statusMsg = "The file ".$fileName. " has been uploaded successfully.";

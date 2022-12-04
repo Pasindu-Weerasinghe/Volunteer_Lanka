4521ae49e7111c7ce984c87a1243539ec6bde135
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2022-12-04 15:47:53 +0530

    changes

--- change_password.php
+++ change_password.php
@@ -1,8 +1,13 @@
 <?php
 require 'conn.php';
 session_start();
-$error=NULL;
+$error = NULL;
 $uid = $_SESSION['uid'];
+
+if (!isset($_SESSION['uid'])) {
+    header("Location: login.php");
+}
+include 'Navbar/navbar_log.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,50 +21,48 @@ $uid = $_SESSION['uid'];
 </head>
 
 <body>
-    <form action="change_password.php" method="POST">
+    <div class="main" id="main">
+        <form action="change_password.php" method="POST">
 
-        <lable>Current Password</lable><br>
-        <input type="text" name="current"><br>
-        <lable>New Password</lable><br>
-        <input type="text" name="new"><br>
-        <lable>Comfirm Password</lable><br>
-        <input type="text" name="confirm"><br><br>
-        <button type="submit" name="submit">Save Password</button>
-        <button type="submit"> <a href="profile_sponsor.php">Back</a> </button>
+            <lable>Current Password</lable><br>
+            <input type="text" name="current"><br>
+            <lable>New Password</lable><br>
+            <input type="text" name="new"><br>
+            <lable>Comfirm Password</lable><br>
+            <input type="text" name="confirm"><br><br>
+            <button type="submit" name="submit">Save Password</button>
+            <button type="submit"> <a href="profile_sponsor.php">Back</a> </button>
 
-    </form>
+        </form>
 
-    <?php
-    $sql = "SELECT * FROM user WHERE U_ID='" . $uid . "'";
-    $result = mysqli_query($conn, $sql);
-    while ($row = $result->fetch_assoc())
-        $cu_pw = $row['Password'];
+        <?php
+        $sql = "SELECT * FROM user WHERE U_ID='" . $uid . "'";
+        $result = mysqli_query($conn, $sql);
+        while ($row = $result->fetch_assoc())
+            $cu_pw = $row['Password'];
+        if (isset($_POST['submit'])) {
+            $password = $_POST['current'];
+            $new = $_POST['new'];
+            $confirm = $_POST['confirm'];
+            $verify = password_verify($password, $cu_pw);
 
-   
-
-    if (isset($_POST['submit'])) {
-        $password = $_POST['current'];
-        $new = $_POST['new'];
-        $confirm = $_POST['confirm'];
-        $verify = password_verify($password, $cu_pw);
-
-        if ($password == $verify) {
-            if ($new == $confirm) {
-                $new1 = password_hash($new, PASSWORD_BCRYPT);
-                $sql="UPDATE user SET Password='".$new1."' WHERE U_ID='".$uid."'";
-                $result=mysqli_query($conn,$sql);
-                $error = "Password Updated Successfully";
+            if ($password == $verify) {
+                if ($new == $confirm) {
+                    $new1 = password_hash($new, PASSWORD_BCRYPT);
+                    $sql = "UPDATE user SET Password='" . $new1 . "' WHERE U_ID='" . $uid . "'";
+                    $result = mysqli_query($conn, $sql);
+                    $error = "Password Updated Successfully";
+                } else {
+                    $error = "password did not match";
+                }
             } else {
-                $error = "password did not match";
+                $error = "OLD PW is not match";
             }
-        } else {
-            $error = "OLD PW is not match";
         }
-    }
-    ?>
+        ?>
 
-    <label class="error"><?php echo $error ?></label>
-
+        <label class="error"><?php echo $error ?></label>
+    </div>
 </body>
 
 </html>
--- publish_advertisment.php
+++ publish_advertisment.php
@@ -21,30 +21,33 @@ include 'Navbar/navbar_log.php';
 
 <body>
     <div class="main" id="main">
-        <h2>Published Advertiesment</h2><br />
+        <h1>Published Advertiesment</h1><br />
         <p>You can publish your company's adverisement from here. It will be sent to our admins and get published after they accept</p> <br />
 
 
-        <p><strong>Upload Advertiesment</strong> </p2><br />
+        <p class="para"><strong><h2>Upload Advertiesment</h2></strong> </p2><br />
 
         <div class="container-image">
 
 
             <form action="upload_image.php" method="post" enctype="multipart/form-data">
 
-                <label for="photo"><b>Add Photos</b></label>
+                <label for="photo"><b>Add Photos: </b></label>
                 <input type="file" name="file[]" multiple="multiple"><br /><br />
+                <!-- <div class="image2" > -->
+                    <img class="image2" src="cards/img1.jpg"> <br><br>
+                <!-- </div> -->
 
-                <label for="des"><b>Description</b></label>
-                <input type="text" name="des" required>
+                <label for="des"><b>Description: </b></label>
+                <input class="des" type="text" name="des" required>
                 <!-- <p><strong>Description</strong><textarea rows="10" cols="70" name="comment" placeholder="Enter here!"></textarea> </p2><br /> -->
                 <br><br><br><br><br>
-                <button class="btn1">Cancel</button>
-                <button class="btn2" name="request">Send Request</button>
+                <button class="btn4"><a href="publish_advertisment.php" >Cancel</a></button>
+                <button class="btn4" name="request">Send Request</button>
             </form>
 
 
-            <!-- <img src="cards/img1.jpg"> -->
+             <!-- <img src="cards/img1.jpg">  -->
         </div>
 
         <br>

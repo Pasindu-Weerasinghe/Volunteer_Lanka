ce747bbe204fb60b8206278329d252aef63c0db3
Binula Rasanjith <binula28@gmail.com>
2022-12-12 20:58:15 +0530

    all files deleted

--- Navbar/navbar.js
+++
@@ -1,3 +0,0 @@
-function toggleMobileMenu(menu) {
-    menu.classList.toggle('open');
-}
--- sidenav/sidenav.js
+++
@@ -1,9 +0,0 @@
-function openSideMenu(){
-  document.getElementById('side-menu').style.width = '300px';
-  document.getElementById('main').style.marginLeft = '300px';
-}
-
-function closeSideMenu(){
-  document.getElementById('side-menu').style.width = '0';
-  document.getElementById('main').style.marginLeft = '0';
-}

13a6271ec1b3d30ae39a38a946a68af87425bdbe
Binula Rasanjith <binula28@gmail.com>
2022-12-15 21:56:49 +0530

    changes

--- views/Organizer/Home.php
+++ views/Organizer/Home.php
@@ -19,53 +19,57 @@ if (!isset($_SESSION['uid'])) {
 <body>
 
     <?php include 'views/includes/navbar_log.php'; ?>
-    
+
     <div class="main" id="main">
 
         <h2>Upcoming Projects</h2><br /><br />
         <section class="container">
             <div class="card">
-                <div class="card-image card1">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img1.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
                 <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
             <div class="card">
-                <div class="card-image card2">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img2.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
                 <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
             <div class="card">
-                <div class="card-image card3">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img3.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
                 <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
-        </section>
-        <hr><br />
+        </section><br />
         <h2>Completed Projects</h2><br /><br />
         <section class="container">
             <div class="card">
-                <div class="card-image card1">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img1.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
-                <a class="btn" href="">View</a>
+                <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
             <div class="card">
-                <div class="card-image card2">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img2.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
-                <a class="btn" href="">View</a>
+                <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
             <div class="card">
-                <div class="card-image card3">
+                <div class="card-image">
+                    <img src="<?php echo BASE_URL ?>public/images/card-img3.jpg" id="card-img">
                 </div>
                 <h2>Project Name</h2>
-                <a class="btn" href="">View</a>
+                <a class="btn" href="<?php echo BASE_URL . 'organizer/view_project/' . $pid; ?>">View</a>
             </div>
         </section>
-
     </div>
 
 </body>

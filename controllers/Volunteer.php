800a6802e8823cca8b91edcfc875d310c4f0fa10
Nethmie <nethimanya2000@gmail.com>
2023-02-09 11:25:27 +0530

    profile

--- controllers/Volunteer.php
+++ controllers/Volunteer.php
@@ -18,12 +18,6 @@ class Volunteer extends User
             $this->prImage[$pid] = $this->model->getProjectImage($pid);
         }
 
-        // $this->comProjects = $this->model->cardsVolunteer();
-        // foreach ($this->comProjects as $comProject) {
-        //     $pid = $comProject['P_ID'];
-        //     $this->comImage[$pid] = $this->model->getProjectImage($pid);
-        // }
-
         $this->loadModel('Ad');
         $this->ads = $this->model->getAds();
 

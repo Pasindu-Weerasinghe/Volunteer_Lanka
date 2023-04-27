d141cee5802e67bfba0250fc6b023844e211dc95
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-04-27 22:06:57 +0530

    change

--- views/Sponsor/changeProfile.php
+++ views/Sponsor/changeProfile.php
@@ -19,45 +19,44 @@ if (!isset($_SESSION['uid'])) {
 <body>
     <?php include 'views/includes/navbar_log.php' ?>
     <div class="main" id="main">
-        <div class="form">
+        <div class="form1">
             <div class="row1">
-                <div class="column1">
-                    <form class="form2" action="<?php echo BASE_URL; ?>Sponsor/changeProfilePic" method="post" enctype="multipart/form-data">
-
-                        <table>
-                            <tr>
-                                <td><?php echo $this->user['Name']; ?></td>
-                            </tr>
-                        </table>
-                        <img class="image" src="<?php echo BASE_URL ?>public/images/<?php echo $this->profile['Photo'] ?>" alt=""><br><br>
+                <div>
+                    <table>
+                        <tr>
+                            <td><?php echo $this->user['Name']; ?></td>
+                        </tr>
+                    </table>
+                    <img class="image" src="<?php echo BASE_URL ?>public/images/<?php echo $this->profile['Photo'] ?>" alt=""><br><br>
+                    <form class="column1" action="<?php echo BASE_URL; ?>Sponsor/changeProfilePic" method="post" enctype="multipart/form-data">
                         <input type="file" name="profilepic"><br>
                         <input type="submit" value="Save">
                     </form>
                 </div>
-                <div class="column2" id="col2">
-
-                    <label for="uname"><b>E-Mail</b></label><br>
-                    <input type="text" name="uname" value="<?php echo $this->profile['Email'] ?>" readonly><br>
-
-                    <label for="uname"><b>Name</b></label><br>
-                    <input type="text" name="uname" value="<?php echo $this->user['Name']; ?>"" > <br>
-
-                    <label for=" about"><b>Contact Number</b></label><br>
-                    <input type="text" name="cNumber" value="<?php echo $this->user['Contact']; ?>"><br>
-
-                    <label for="des"><b>Address</b></label><br>
-                    <input type="text" name="cNumber" value="<?php echo $this->user['Address']; ?>"><br>
-
-                    <button class="updatebt" name="Update" id="complain-btn">Update Profile</button>
-
-                </div>
             </div>
 
-</div>
+            <form class="column2">
+
+                <label for="uname"><b>E-Mail</b></label><br>
+                <input type="text" name="uname" value="<?php echo $this->profile['Email'] ?>" readonly><br>
+
+                <label for="uname"><b>Name</b></label><br>
+                <input type="text" name="uname" value="<?php echo $this->user['Name']; ?>"" > <br>
+
+                        <label for=" about"><b>Contact Number</b></label><br>
+                <input type="text" name="cNumber" value="<?php echo $this->user['Contact']; ?>"><br>
+
+                <label for="des"><b>Address</b></label><br>
+                <input type="text" name="cNumber" value="<?php echo $this->user['Address']; ?>"><br>
+
+                <button class="updatebt" name="Update" >Update Profile</button>
+
+            </form>
+
+        </div>
 
     </div>
 
 </body>
-
 
 </html>

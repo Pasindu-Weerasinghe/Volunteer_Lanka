1544e5c71785906124d27a8e7031b7ba3663fb0d
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-05-12 20:56:30 +0530

    edit slide show

--- views/Sponsor/view_projects_sponsor.php
+++ views/Sponsor/view_projects_sponsor.php
@@ -1,3 +1,7 @@
+<?php
+$pid = $this->project['P_ID'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,40 +19,66 @@
     <div class="main" id="main">
         <h2><?php echo $this->project['Name'] ?></h2><br /><br />
         <div class="container">
-            <div class="container-image">
-                <?php foreach ($this->images as $image) { ?>
-                    <img src="<?php echo BASE_URL ?>public/images/pr_images/<?php echo $image['Image']; ?>">
+            <div class="slider">
+                <?php if (count($this->images) > 1) { ?>
+                    <?php foreach ($this->images as $index => $image) : ?>
+                        <span id="slide-<?php echo $index + 1 ?>"></span>
+                    <?php endforeach; ?>
+                <?php } ?>
+                
+                <div class="image-container">
+                    <?php foreach ($this->images as  $index => $image) : ?>
+                        <img src="<?php echo BASE_URL ?>public/images/pr_images/<?php echo $image['Image'] ?>" class="slide" width="500" height="300" />
+                    <?php endforeach; ?>
+                </div>
+                <?php if (count($this->images) > 1) { ?>
+                    <div class="buttons">
+                        <?php foreach ($this->images as $index => $image) : ?>
+                            <a href="#slide-<?php echo $index + 1 ?>"></a>
+                        <?php endforeach; ?>
+                    </div>
                 <?php } ?>
             </div>
-            <div class="wrapper">
-                <div class="left-div">
-                    <label for="">Date</label><br>
-                    <label for="">Time</label><br>
-                    <label for="">Venue</label><br>
-                    <label for="">Number of Volunteers</label><br>
-                    <label for="">Description</label><br>
+
+
+            <div class="details-container">
+                <div class="row">
+                    <label for="date">Date</label>
+                    <p id="date"><?php echo $this->project['Date'] ?></p>
                 </div>
 
-                <div class="right-div">
-                    <label id="data"><?php echo $this->project['Date'] ?></label><br>
-                    <label id="data"><?php echo $this->project['Time'] ?></label><br>
-                    <label id="data"><?php echo $this->project['Venue'] ?></label><br>
-                    <label id="data"><?php echo $this->project['No_of_volunteers'] ?></label><br>
-                    <label id="data"><?php echo $this->project['Description'] ?></label><br>
+                <div class="row">
+                    <label for="time">Time</label>
+                    <p id="time"><?php echo $this->project['Time'] ?></p>
+                </div>
 
 
+
+                <div class="row">
+                    <label for="venue">Venue</label>
+                    <p id="venue"><?php echo $this->project['Venue'] ?></p>
+                </div>
+
+                <div class="row">
+                    <label for="volunteers">Volunteers</label>
+                    <p id="volunteers"><?php echo $this->project['No_of_volunteers'] ?></p>
+                </div>
+
+                <div class="row">
+                    <label for="description">Description</label>
+                    <p id="description"><?php echo $this->project['Description'] ?></p>
                 </div>
             </div>
+
             <div class="btn-area">
                 <button onclick="history.back()" class="btn">Back</button>
                 <a href=" <?php echo BASE_URL ?> sponsor/publish_advertisement/<?php echo $this->project['P_ID'] ?>"> <button class="btn" id="publish-btn"> Publish Advertiesment</button>
             </div>
-
         </div>
     </div>
-    <!-- <?php include 'views/includes/footer.php'; ?> -->
     <input type="hidden" name="ad-published" id="ad-published" value="<?php echo json_encode($this->ad_published) ?>">
 </body>
+
 <script>
     const adPublished = JSON.parse(document.getElementById('ad-published').value);
     console.log(adPublished);
@@ -58,6 +88,18 @@
         publishBtn.style.backgroundColor = 'grey';
         publishBtn.title = "You cannot publish. Because you already published for this project.";
     }
+
+    const descriptionElement = document.getElementById('description');
+    const descriptionHeight = descriptionElement.offsetHeight;
+    const rowElement = descriptionElement.parentElement;
+    rowElement.style.height = `${descriptionHeight + 10}px`;
+    descriptionElement.style.height = '100%';
+
+    const venueElement = document.getElementById('venue');
+    const venueHeight = venueElement.offsetHeight;
+    const venueRowElement = venueElement.parentElement;
+    venueRowElement.style.height = `${venueHeight + 10}px`;
+    venueElement.style.height = '100%';
 </script>
 
 </html>
